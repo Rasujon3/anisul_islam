3d5e2b769e2ac210451b09e995e63047e1df5bf1
sujon <rasujon3@gmail.com>
2021-04-01 01:53:30 +0600

    end 2.5

--- app/src/main/java/com/sujon/mytextviewdemo/MainActivity.java
+++ app/src/main/java/com/sujon/mytextviewdemo/MainActivity.java
@@ -3,12 +3,22 @@ package com.sujon.mytextviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+    //private TextView ageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = findViewById(R.id.nameTextViewId);
+        //ageTextView = findViewById(R.id.ageTextViewId);
+
+        nameTextView.setText("RA Sujon");
+        //ageTextView.setText("24 years old");
     }
 }
